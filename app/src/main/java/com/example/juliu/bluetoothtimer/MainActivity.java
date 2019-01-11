109ebb0791b33070c92710422f8b86263af15724
jkliss <julius.kliss@gmail.com>
2019-01-11 19:14:40 +0100

    update

--- app/src/main/java/com/example/juliu/bluetoothtimer/MainActivity.java
+++ app/src/main/java/com/example/juliu/bluetoothtimer/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public boolean stop = false;
+    public boolean recentlyChanged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
                 final EditText editText = (EditText) findViewById(R.id.editText);
                 String gottext = String.valueOf(editText.getText());
                 try{
-                    int number = Integer.parseInt(gottext);
-                    startTimer(number*60);
+                    double snum = Double.parseDouble(gottext);
+                    System.out.println(snum);
+                    long number = (long) (snum*60);
+                    startTimer(number);
                 } catch (Exception ex){
                     ex.getStackTrace();
                 }
@@ -44,11 +49,37 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(text);
             }
         });
+
+        Switch mySwitch = (Switch) findViewById(R.id.switch1);
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                recentlyChanged = true;
+                if(isChecked){
+                    //Enable bluetooth
+                    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    if (mBluetoothAdapter != null && !mBluetoothAdapter.isEnabled()) {
+                        mBluetoothAdapter.enable();
+                    }
+                } else {
+                    //Disable bluetooth
+                    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+                        mBluetoothAdapter.disable();
+                    }
+                }
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+            }
+        });
+
+        periodicBluetoothCheck();
         textView.setText("");
     }
 
-    void startTimer(int input){
+    void startTimer(long input){
         final TextView textView = (TextView) findViewById(R.id.Timerfield);
+        final Switch myswitch = (Switch) findViewById(R.id.switch1);
 
         new CountDownTimer(1000*input, 1000) {
 
@@ -73,8 +104,36 @@ public class MainActivity extends AppCompatActivity {
                 BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
                 if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
                     mBluetoothAdapter.disable();
+                    myswitch.setChecked(false);
                 }
             }
         }.start();
+    }
+
+    void periodicBluetoothCheck(){
+        new CountDownTimer(Integer.MAX_VALUE, 10000) {
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            final Switch mySwitch = (Switch) findViewById(R.id.switch1);
+
+            public void onTick(long millisUntilFinished) {
+                if (mBluetoothAdapter != null) {
+                    if(recentlyChanged) {
+                        //skip check iteration if changed recently
+                        recentlyChanged = false;
+                    } else if(mBluetoothAdapter.isEnabled()){
+                        mySwitch.setChecked(true);
+                    } else {
+                        mySwitch.setChecked(false);
+                    }
+                } else {
+                    mySwitch.setChecked(false);
+                }
+            }
+
+            public void onFinish() {
+
+            }
+        }.start();
+
     }
 }
